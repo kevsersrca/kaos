4cb1808fffcbb6754753feb1441fceaeaaf82c47
root <root@localhost.localdomain>
2018-06-09 08:24:45 -0400

    My Operation System Bootloader

---
+++ kernel.cpp
@@ -0,0 +1,30 @@
+
+void printf(char* str) {
+    static unsigned short* VideoMemory  = (unsigned short*)0xb8000;
+    
+    for (int i=0; str[i] != '\0'; ++i) 
+        VideoMemory[i]=(VideoMemory[i] & 0xFF00)  | str[i];
+    
+}
+
+typedef void (*constructor) ();
+
+extern "C" constructor start_ctors;
+
+extern "C" constructor end_ctors;
+
+extern "C" void callConstructor(void* multiboot_structure,unsigned int /*multiboot_magic*/) {
+    
+    for(constructor* i = &start_ctors; i != &end_ctors;i++) {
+        (*i)();
+        
+    }
+}
+
+
+extern "C" void kernelMain() {
+        
+        printf("Merhaba Dünya");
+        
+        while(1);
+}
